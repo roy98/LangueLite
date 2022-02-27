af530c77c1f19909a41b29f38baf070a4bb53e8b
roy98 <roy.ndjanfa@vifbox.com>
2022-02-28 00:31:05 +0100

    discover our languages

---
+++ LangueLite/Pages/Tchat.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LangueLite.Pages
+{
+    public class TchatModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
