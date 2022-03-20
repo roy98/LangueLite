8bc6aa606f8b2af0aa93d53cbbcc3f088aea60ef
roy98 <roy.ndjanfa@vifbox.com>
2022-03-20 17:36:07 +0100

    tchat v1 and signup v1

---
+++ LangueLite/Pages/Signup.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LangueLite.Pages
+{
+    public class SignupModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
