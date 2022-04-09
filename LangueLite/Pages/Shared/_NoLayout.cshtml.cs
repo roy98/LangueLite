ca70b08c48c566753dbb372dde2dbc47e0df04fa
Roy Christo <christoroybiaga@gmail.com>
2022-04-09 00:16:40 +0000

    no layout pages

---
+++ LangueLite/Pages/Shared/_NoLayout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LangueLite.Pages.Shared
+{
+    public class _NoLayoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
